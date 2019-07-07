6455baa0d4df5350b42944ce05b2665069a9263f
Gunsum117 <juniwamoto1117@gmail.com>
2019-07-07 12:38:56 +0000

    add ルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -15,12 +15,23 @@ Rails.application.routes.draw do
   }
 
   namespace :admin do
-
-
+  	resources :users,       only:[:index, :show, :edit, :update, :destroy]
+  	resources :films,		only:[:new, :create, :index, :show, :edit, :update, :destroy]
+  	resources :tags,		only:[:create, :index, :show, :update]
+  	resources :reviews,		only:[:edit, :update]
   end
 
 
-  resouces :users,  only:[:show :edit :]
+  resources :users,  		only:[:show, :edit, :update, :resign, :destroy]
+  resources :relationships,	only:[:create, :destroy]
+  resources :films,			only:[:new, :create, :index, :show, :edit, :update, :destroy]
+  resources :tags,			only:[:create, :index, :show, :update]
+  resources :reviews,		only:[:new, :create, :edit, :update]
+  resources :good_reviews,	only:[:create, :destroy]
+  resources :wishes,		only:[:create, :destroy]
+  resources :dones,			only:[:create, :destroy]
+  resources :mybests,		only:[:create, :destroy]
+  resources :contacts,		only:[:new, :create, :destroy]
 
 
 end
--- db/migrate/20190707101133_create_good_reviews.rb
+++ db/migrate/20190707101133_create_good_reviews.rb
@@ -1,8 +1,8 @@
 class CreateGoodReviews < ActiveRecord::Migration[5.2]
   def change
     create_table :good_reviews do |t|
-      t.string :review_id
-      t.string :user_id
+      t.string :review_id,	null: false
+      t.string :user_id,	null: false
 
       t.timestamps
     end
