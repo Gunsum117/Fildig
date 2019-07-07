97fd4a20ed561afe5b630da7036cf64505bb2e91
Gunsum117 <juniwamoto1117@gmail.com>
2019-07-07 12:10:11 +0000

    add devise周りのファイル追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,43 +1,26 @@
 Rails.application.routes.draw do
-  get 'contacts/new'
-  get 'contacts/create'
-  get 'contacts/show'
-  get 'mybests/create'
-  get 'mybests/destroy'
-  get 'dones/create'
-  get 'dones/destroy'
-  get 'wishes/create'
-  get 'wishes/destroy'
-  get 'good_reviews/create'
-  get 'good_reviews/destroy'
-  get 'reviews/new'
-  get 'reviews/create'
-  get 'reviews/edit'
-  get 'reviews/update'
-  get 'tags/create'
-  get 'tags/index'
-  get 'tags/show'
-  get 'tags/update'
-  get 'relationships/create'
-  get 'relationships/destroy'
-  get 'films/new'
-  get 'films/create'
-  get 'films/index'
-  get 'films/show'
-  get 'films/edit'
-  get 'films/update'
-  get 'films/destroy'
-  get 'relationship/create'
-  get 'relationship/destroy'
-  get 'users/show'
-  get 'users/edit'
-  get 'users/update'
-  get 'users/resign'
-  get 'users/destroy'
-  devise_for :admins
-  devise_for :users
+
+  root
+
+  devise_for :admins, controllers: {
+    sessions: 'admins/sessions',
+    passwords: 'admins/passwords',
+    registrations: 'admins/registrations'
+  }
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations'
+  }
+
+  namespace :admin do
 
 
-  
+  end
+
+
+  resouces :users,  only:[:show :edit :]
+
 
 end
