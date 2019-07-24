a9cc3272be7d95d9d022a0ad61bd78656e27f2fb
Gunsum117 <juniwamoto1117@gmail.com>
2019-07-24 19:29:58 +0000

    add user_editの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   before_action :authenticate_user!, only: [:show, :edit, :update, :resign, :destroy, :follows, :followers]
+  before_action :correct_user, only: [:edit, :update, :destroy]
 
   def show
     @user = User.find(params[:id])
@@ -10,9 +11,18 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    user = User.find(params[:id])
+    if user.update(user_params)
+      flash[:success] = "プロフィールを編集しました！"
+      redirect_to user_path(user)
+    else
+      flash[:warning] = "プロフィールの編集に失敗しました！"
+      redirect_to edit_user_path(user)
+    end
   end
 
   def resign
@@ -30,5 +40,15 @@ class UsersController < ApplicationController
     user = User.find(params[:id])
     @users = user.followers
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :image)
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to user_path(@user) unless @user == current_user
+    end
 
 end
