c0ecfc432e1fd25cf1731576775663ec264eb9a1
Gunsum117 <juniwamoto1117@gmail.com>
2019-07-26 08:02:47 +0000

    add tag検索の実装

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -4,6 +4,9 @@ class FilmsController < ApplicationController
 
   def index
     @films = Film.all
+    if params[:tag_name]
+      @films = @films.tagged_with("#{params[:tag_name]}")
+    end
   end
 
   def show
