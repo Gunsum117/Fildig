7ca33fe4b9e9a45ecc9216e11d2cece080ed28ae
Gunsum117 <juniwamoto1117@gmail.com>
2019-07-28 13:57:36 +0000

    add wish/done/mybestのajax化

--- app/controllers/dones_controller.rb
+++ app/controllers/dones_controller.rb
@@ -3,17 +3,15 @@ class DonesController < ApplicationController
   before_action :authenticate_user!, only: [:create, :destroy]
 
   def create
-	film = Film.find(params[:film_id])
-	done = current_user.dones.new(film_id: film.id)
+	@film = Film.find(params[:film_id])
+	done = current_user.dones.new(film_id: @film.id)
 	done.save
-	redirect_to film_path(film)
   end
 
   def destroy
-	film = Film.find(params[:film_id])
-	done = current_user.dones.find_by(film_id: film.id)
+	@film = Film.find(params[:film_id])
+	done = current_user.dones.find_by(film_id: @film.id)
 	done.destroy
-	redirect_to film_path(film)
   end
 
 end
