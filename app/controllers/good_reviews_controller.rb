b4faac49cc5252d0a8f68522d08aae9a10ade04e
Gunsum117 <juniwamoto1117@gmail.com>
2019-07-28 16:27:28 +0000

    add good_reviewのajax化

--- app/controllers/good_reviews_controller.rb
+++ app/controllers/good_reviews_controller.rb
@@ -6,13 +6,11 @@ class GoodReviewsController < ApplicationController
 	@review = Review.find_by(id: params[:review_id])
 	good_review = current_user.good_reviews.new(review_id: @review.id)
 	good_review.save
-	redirect_to film_path(@review.film_id)
   end
 
   def destroy
 	@review = Review.find_by(id: params[:review_id])
 	good_review = current_user.good_reviews.find_by(review_id: @review.id)
 	good_review.destroy
-	redirect_to film_path(@review.film_id)
   end
 end
